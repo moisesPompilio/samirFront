84537f8a57687d8f9c393609312d18ed6fa9d28c
luanpaivab <luanpb0511@gmail.com>
2022-01-06 18:34:17 -0300

    :package: add alguns arquivos de src

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid class="pa-0">
+        <transition-group>
+          <div key="page-template">
+            <toolbar />
+          </div>
+          <div key="page">
+            <Content />
+          </div>
+        </transition-group>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+// import Login from "./pages/Login";
+import Content from "./components/Content.vue";
+import Toolbar from "./components/Toolbar.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    // Login,
+    Toolbar,
+    Content,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
