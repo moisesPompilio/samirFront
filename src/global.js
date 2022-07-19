27481554da5b09f36e4201d684c92dda31adff94
Moises Pompilio <moisesjtmed@gmail.com>
2022-07-19 18:03:26 -0300

    ajuste da url da api

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 export const userKey = '__gbelpa_user'
-export const baseApiUrl = 'http://localhost:8888/'
-//export const baseApiUrl = 'https://samir-calculadora.herokuapp.com/'
+//export const baseApiUrl = 'http://localhost:8888/'
+export const baseApiUrl = 'https://samir-calculadora.herokuapp.com/'
 
 export default { baseApiUrl, userKey }
